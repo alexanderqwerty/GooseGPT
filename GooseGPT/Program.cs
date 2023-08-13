b5834bf310b9b5d733f9bb570935af061e4590ad
Alex <duducensash@gmail.com>
2023-08-14 00:27:38 +0300

    Its work!!!

---
+++ GooseGPT/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using GooseShared;
+using System.IO;
+using System.Net;
+using System.Threading.Tasks;
+using OpenAI_API;
+
+
+namespace GooseGPT
+{
+    public class ModMain : IMod
+    {
+        const string OpenAIApiKey = "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";
+        static readonly string Path = Directory.GetCurrentDirectory() + @"\Assets\Text\NotepadMessages";
+
+        const string prompt =
+            "Напиши несколько фраз на бодобие \"Хорошей работы\" \"я делаяю это специально\" Напитши один вариант";
+
+        const string model = "text-davinci-003";
+
+
+        public void Init()
+        {
+            InjectionPoints.PostTickEvent += PreTick;
+        }
+
+        private static bool _flag = true;
+
+        private static async void updateText()
+        {
+            ServicePointManager.Expect100Continue = true;
+            ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
+
+            var openAI = new OpenAIAPI(new APIAuthentication(OpenAIApiKey));
+            var conversation = openAI.Chat.CreateConversation();
+            conversation.AppendUserInput("напиши только \"Да\"");
+            var response = await conversation.GetResponseFromChatbotAsync();
+            using var writer = new StreamWriter(Path + @"\message.txt");
+            Console.WriteLine(response);
+            writer.Close();
+        }
+
+        private static void PreTick(GooseEntity goose)
+        {
+            if (_flag && goose.currentTask == 3)
+            {
+                Task.Run(updateText);
+                _flag = false;
+            }
+
+            if (!_flag && goose.currentTask == 0)
+            {
+                Console.WriteLine("Start");
+                updateText();
+                _flag = true;
+            }
+        }
+    }
+}
---
+++ GooseGPT/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("GooseGPT")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("GooseGPT")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("47E43635-7120-4536-A907-F859C65B9100")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
